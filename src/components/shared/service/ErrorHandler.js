7a46b3022cbeef7b24c17c40189a5f9b6cc2ca04
gaston <gfrias@garbarino.com.ar>
2019-05-16 02:12:33 -0300

    fix for bootstrap toastr

--- src/components/shared/service/ErrorHandler.js
+++ src/components/shared/service/ErrorHandler.js
@@ -3,16 +3,20 @@
 import toastr from 'toastr'
 
 export const successToaster = (title, desc) => {
+  toastr.options.toastClass = 'toastr';
   return (toastr.success(desc, title))
 }
 export const errorToaster = (title, desc) => {
+  toastr.options.toastClass = 'toastr';
   return (toastr.error(desc, title))
 }
 
 export const warnToaster = (title, desc) => {
+  toastr.options.toastClass = 'toastr';
   return (toastr.warning(desc, title))
 }
 
 export const infoToaster = (title, desc) => {
+  toastr.options.toastClass = 'toastr';
   return (toastr.info(desc, title))
 }
