a96be4ccc1711c04f3bc5290511ff06f5184bd8b
gaston <gfrias@garbarino.com.ar>
2019-06-18 18:45:07 -0300

    mp integration

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,8 +1,9 @@
 <template>
     <div>
-        <form action="http://localhost:3000/api/payment" method="POST" ref="mpForm">
+        <form :action="mpURL" method="POST" ref="mpForm" >
         </form>
         <h6>{{amount}}</h6>
+        <h6>{{mpURL}}</h6>
     </div>
 </template>
 <script>
@@ -12,7 +13,8 @@ export default {
     name: 'Payment',
     data () {
         return {
-            amount: 0
+            amount: 0,
+            mpURL: ''
         }
     },
     computed: mapState(['cartProducts']),
@@ -22,31 +24,14 @@ export default {
             this.amount += product.price;
         });
 
+        this.mpURL=`${process.env.VUE_APP_BASE_URL}/payment`;
+
         let foo = document.createElement('script');    
         foo.setAttribute("src","https://www.mercadopago.com.ar/integrations/v1/web-tokenize-checkout.js");
         foo.setAttribute("data-transaction-amount", this.amount);
         foo.setAttribute("data-public-key", "TEST-ad30a504-5dc3-45c9-a713-495e722ef7f8");
         foo.setAttribute("data-elements-color", "#c0392b");
         this.$refs.mpForm.appendChild(foo);
-    },
-    methods: {
-        sub: function(event){
-            console.log("entra al submit");
-            console.log(event);
-            axios
-                .post(`${process.env.VUE_APP_BASE_URL}/payment`, {})
-                .then(response => {
-                this.showLoader = false
-                this.ADD_LOGGED_USER(response.data[0])
-                event.target.reset()
-                this.$router.push(this.$route.query.from || '/')
-                })
-                .catch(error => {
-                this.showLoader = false
-                errorToaster('Invalid Credentials', '')
-                console.log(error)
-                })
-        }
     }
 }
 </script>
